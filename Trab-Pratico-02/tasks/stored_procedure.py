bbb708b75664cc4ee58ccfd9a87b1e0f56d70d01
Marcelo <marcelomeirelesmarquesfilho@hotmail.com>
2023-11-15 13:45:43 -0300

    Stored procedure, so que falta testar

--- Trab-Pratico-02/tasks/stored_procedure.py
+++ Trab-Pratico-02/tasks/stored_procedure.py
@@ -10,11 +10,19 @@ try:
     # parâmetro uma data e retorna o id e nome do empregado que participou de mais movimentações naquela
     # combinação Ano/Mês;
     cursor.execute("""CREATE OR REPLACE PROCEDURE empregado_do_mes (date DATE)
-                        LANGUAGE SQL AS $$
-                        SELECT E.id_emp, E.nome FROM (CELITO COMPLETA)""")
+                    LANGUAGE SQL AS $$
+                        SELECT E.id_emp, E.nome 
+                        FROM Empregados e 
+                        JOIN Movimentacoes_Empregados me ON e.id_emp = me.id_emp
+                        JOIN Movimentacoes a ON a.id_mov = me.id_mov
+                        WHERE a.data>= data_mov AND a.data < data_mov + INTERVAL '1 month' 
+                        GROUP BY e.id_emp, e.nome 
+                        ORDER BY COUNT(*) DESC 
+                        LIMIT 1
+                    $$""")
 
     # Chama o Stored Procedure
-    cursor.callproc("empregado_do_mes", ("2023-10-01"))
+    cursor.execute("SELECT id_emp, nome FROM empregado_do_mes('2023-10-01')")
     empregado_do_mes = cursor.fetchall()
 
     # Printa o Stored Procedure
